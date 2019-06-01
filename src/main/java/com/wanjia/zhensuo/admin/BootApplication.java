1c8fa40de7b0c5e14c2252d827b31673d5f71b7a
zmrush <zmhust88@gmail.com>
2019-06-01 10:44:20 +0800

    mapper scan

--- src/main/java/com/wanjia/zhensuo/admin/BootApplication.java
+++ src/main/java/com/wanjia/zhensuo/admin/BootApplication.java
@@ -1,9 +1,11 @@
 package com.wanjia.zhensuo.admin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.wanjia.zhensuo.admin.mapper")
 public class BootApplication {
 
     public static void main(String[] args){
