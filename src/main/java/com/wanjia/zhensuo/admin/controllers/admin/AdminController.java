5169f6e42377faf4de76b621a2d3058891880c1c
zmrush <zmhust88@gmail.com>
2019-06-01 10:49:55 +0800

    add controller

---
+++ src/main/java/com/wanjia/zhensuo/admin/controllers/admin/AdminController.java
@@ -0,0 +1,9 @@
+package com.wanjia.zhensuo.admin.controllers.admin;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/admin/")
+@RestController
+public class AdminController {
+} 
---
+++ src/main/java/com/wanjia/zhensuo/admin/controllers/patient/PatientController.java
@@ -0,0 +1,9 @@
+package com.wanjia.zhensuo.admin.controllers.patient;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/patient/")
+public class PatientController {
+} 
